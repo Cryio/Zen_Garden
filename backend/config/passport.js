8adc48d1875defd1ebeeea393e6444b57a04ca8d
anmol ranjan <anmol50.ran@gmail.com>
2025-04-23 23:36:50 +0530

    [Oauth2] attempting to further change the logic of the linked account message being displayed with logging

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -10,23 +10,69 @@ passport.use(new GoogleStrategy({
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
-      // Check if user already exists
+      const email = profile.emails[0].value;
+      console.log('Google profile:', {
+        id: profile.id,
+        email: email,
+        displayName: profile.displayName
+      });
+      
+      // First check if user exists with Google ID
       let user = await User.findOne({ googleId: profile.id });
+      console.log('User with Google ID:', user ? {
+        id: user._id,
+        email: user.email,
+        isGoogleUser: user.isGoogleUser,
+        hasPassword: !!user.password
+      } : 'Not found');
       
       if (!user) {
-        // Create new user if doesn't exist
-        user = await User.create({
-          googleId: profile.id,
-          email: profile.emails[0].value,
-          name: profile.displayName,
-          isGoogleUser: true,
-          // Set default values for required fields
-          firstName: profile.name?.givenName || 'Google',
-          lastName: profile.name?.familyName || 'User',
-          gender: 'other', // Default value
-          dob: new Date('2000-01-01'), // Default value
-          password: 'google-auth' // Dummy password for Google users
-        });
+        // If no user with Google ID, check if user exists with email
+        user = await User.findOne({ email: email });
+        console.log('User with email:', user ? {
+          id: user._id,
+          email: user.email,
+          isGoogleUser: user.isGoogleUser,
+          hasPassword: !!user.password
+        } : 'Not found');
+        
+        if (user) {
+          // If user exists with email, link Google account to it
+          console.log('Linking Google account to existing user');
+          // Only set isGoogleUser if it wasn't already a Google user
+          if (!user.isGoogleUser) {
+            user.isGoogleUser = true;
+          }
+          user.googleId = profile.id;
+          await user.save();
+          console.log('Updated user:', {
+            id: user._id,
+            email: user.email,
+            isGoogleUser: user.isGoogleUser,
+            hasPassword: !!user.password
+          });
+        } else {
+          // Create new user if doesn't exist
+          console.log('Creating new Google user');
+          user = await User.create({
+            googleId: profile.id,
+            email: email,
+            name: profile.displayName,
+            isGoogleUser: true,
+            // Set default values for required fields
+            firstName: profile.name?.givenName || 'Google',
+            lastName: profile.name?.familyName || 'User',
+            gender: 'other', // Default value
+            dob: new Date('2000-01-01'), // Default value
+            password: 'google-auth' // Dummy password for Google users
+          });
+          console.log('Created new user:', {
+            id: user._id,
+            email: user.email,
+            isGoogleUser: user.isGoogleUser,
+            hasPassword: !!user.password
+          });
+        }
       }
       
       return done(null, user);
