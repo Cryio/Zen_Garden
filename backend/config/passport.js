700a0d48075801ba6c6f91e98bfdd4c74ce5cd2a
anmol ranjan <anmol50.ran@gmail.com>
2025-04-23 21:32:23 +0530

    [Oauth2] added passport strategy

---
+++ backend/config/passport.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      // Check if user already exists
+      let user = await User.findOne({ googleId: profile.id });
+      
+      if (!user) {
+        // Create new user if doesn't exist
+        user = await User.create({
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          name: profile.displayName,
+          isGoogleUser: true
+        });
+      }
+      
+      return done(null, user);
+    } catch (error) {
+      console.error('passport strategy error: ', error);  
+      return done(error, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  console.log('serializeUser: ', user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+module.exports = passport; 
